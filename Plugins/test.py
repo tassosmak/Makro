89606a107abbc5d7022e35300f2e44f472a1b593
Tassos Makrostergios <86314553+TASMAK@users.noreply.github.com>
2026-01-15 18:13:55 +0200

    test

--- Plugins/test.py
+++
@@ -1,6 +0,0 @@
-from src.utils import add_depend, sys, kernel
-add_depend(str(sys.argv[1]))
-
-from Makro.MakroCore.RendererKit import Renderer as RD
-
-RD.CommandShow(kernel.get_state("enable_audio")).Show()
